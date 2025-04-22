119b3a6cdd9e9fd4bc33a00c0dea1b9e7c4703a2
isatrouxa <07070707isa@gmail.com>
2025-04-21 21:16:56 -0300

    Update and rename dynamic.js to O-Contador-2.html

--- dynamic.js
+++
@@ -1,8 +0,0 @@
-fetch("http://testzin.fwh.is/config.json")
-  .then(response => response.json())
-  .then(config => {
-    // Modificar os elementos da página com base no config
-  })
-  .catch(error => {
-    console.error("Erro ao carregar o config:", error);
-  });
